7daf97cf7df34d47ce5dd08758fa45beb93644de
Alejandro Contreras <alex.mart.contreras@me.com>
2018-11-01 11:39:29 -0500

    Lots of work through chapter 4

--- code/chap02ex.py
+++ code/chap02ex.py
@@ -3,6 +3,15 @@ by Allen B. Downey, available from greenteapress.com
 
 Copyright 2014 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
+
+Write a function called Mode that takes a Hist and returns the most frequent value.
+
+As a more challenging exercise, write a function called AllModes
+that returns a list of value-frequency pairs in descending order of frequency.
+
+Using the variable totalwgt_lb, nvestigate whether first ba- bies are lighter or
+heavier than others. Compute Cohen’s d to quantify the difference between the groups.
+How does it compare to the difference in pregnancy length?
 """
 
 from __future__ import print_function
@@ -15,13 +24,8 @@ import thinkstats2
 
 
 def Mode(hist):
-    """Returns the value with the highest frequency.
-
-    hist: Hist object
-
-    returns: value from Hist
-    """
-    return 0
+    value, key = max([(value, key) for key, value in hist.Items()])
+    return key
 
 
 def AllModes(hist):
@@ -31,8 +35,38 @@ def AllModes(hist):
 
     returns: iterator of value-freq pairs
     """
-    return []
+    return sorted(hist.Items(), key=itemgetter(1), reverse=True)
 
+def WeightDifference(live, firsts, others):
+    """Explore the difference in weight between first babies and others.
+
+    live: DataFrame of all live births
+    firsts: DataFrame of first babies
+    others: DataFrame of others
+    """
+    mean0 = live.totalwgt_lb.mean()
+    mean1 = firsts.totalwgt_lb.mean()
+    mean2 = others.totalwgt_lb.mean()
+
+    var1 = firsts.totalwgt_lb.var()
+    var2 = others.totalwgt_lb.var()
+
+    print('Mean')
+    print('First babies', mean1)
+    print('Others', mean2)
+
+    print('Variance')
+    print('First babies', var1)
+    print('Others', var2)
+
+    print('Difference in lbs', mean1 - mean2)
+    print('Difference in oz', (mean1 - mean2) * 16)
+
+    print('Difference relative to mean (%age points)',
+          (mean1 - mean2) / mean0 * 100)
+
+    d = thinkstats2.CohenEffectSize(firsts.totalwgt_lb, others.totalwgt_lb)
+    print('Cohen d', d)
 
 def main(script):
     """Tests the functions in this module.
@@ -42,14 +76,14 @@ def main(script):
     live, firsts, others = first.MakeFrames()
     hist = thinkstats2.Hist(live.prglngth)
 
-    # test Mode    
+    # test Mode
     mode = Mode(hist)
     print('Mode of preg length', mode)
-    assert mode == 39, mode
+    assert(mode == 39)
 
     # test AllModes
     modes = AllModes(hist)
-    assert modes[0][1] == 4693, modes[0][1]
+    assert(modes[0][1] == 4693)
 
     for value, freq in modes[:5]:
         print(value, freq)
